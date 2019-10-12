95c2d5a1c1707e6c4eb4d7aed1cbe2bee8167a91
Henry Suzukawa <henry.suzukawa@telecom-paris.fr>
2019-10-12 10:44:12 +0200

    Finish interruption vector and remove redundant rule in Makefile.
    + irq.c
    	Finish interruption vector
    
    - Makefile
    	Remove linker instruction

--- td/modules/irq/irq.h
+++ td/modules/irq/irq.h
@@ -1,13 +1,15 @@
 #ifndef IRQ_H
 #define IRQ_H
 
-#include <cmsis_gcc.h>
+#include <stm32l475xx.h>
+#include <stm32l4xx.h>
+#include <stdint.h>
 
-MAKE_DEFAULT_HANDLER(irq_name) (                            \
-    void __attribute__((weak)) (irq_name)##_IRQHandler {    \
+
+#define MAKE_DEFAULT_HANDLER(irq_name)                      \
+    void __attribute__((weak)) irq_name() {  \
         __disable_irq();                                    \
         while(1);                                           \
-    }                                                       \
-)
+    }
 
 #endif // IRQ_H
